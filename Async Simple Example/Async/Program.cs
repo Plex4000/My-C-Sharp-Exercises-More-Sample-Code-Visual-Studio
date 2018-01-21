d37d3e8cd3b557f826cf6144f782a1b4fdac2667
Plex4000 <rajnarayanan2020@gmail.com>
2018-01-20 21:23:16 -0500

    Added a few more code samples

---
+++ Async Simple Example/Async/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class Program
+    {
+        public async void Print()
+        {
+            await SlowWork();
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("AFTER AWAIT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            }
+
+        }
+
+        private async Task SlowWork()
+        {
+            for(int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine(i);
+            }
+           
+        }
+
+        static void Main(string[] args)
+        {
+        
+            Program prog = new Program();
+            prog.Print();
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("MAIN THREAD!");
+            }
+
+
+
+
+        }
+    }
+}
